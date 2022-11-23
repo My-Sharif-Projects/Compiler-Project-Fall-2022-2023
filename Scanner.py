c9c8ff879d745c23acffd1d122500e1edb034b12
ParhamBateni <mp.bateni@gmail.com>
2022-11-23 22:48:32 +0330

    Fix Scanner DFA + Fix Scanner

--- Scanner.py
+++ Scanner.py
@@ -1,8 +1,12 @@
 TOKEN_TYPES = ['NUM', 'ID', 'KEYWORD', 'SYMBOL', 'COMMENT', 'WHITESPACE']
 KEYWORDS = ['if', 'else', 'void', 'int', 'while', 'break', 'switch', 'default', 'case', 'return', 'endif']
-SYMBOLS=[';', ':', ',', '[', ']', '(', ')', '{', '}','+', '-', '*', '<', '=','/']
-WHITESPACES=[' ', '\n', '\r', '\t', '\v', '\f']
-ACCEPTABLE_CHARS=[chr(i) for i in range(65,65+26)]+[chr(i) for i in range(97,97+26)]+[chr(i) for i in range(48,48+10)]+SYMBOLS+WHITESPACES
+SYMBOLS = [';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '*', '<', '=', '/']
+WHITESPACES = [' ', '\n', '\r', '\t', '\v', '\f']
+ACCEPTABLE_CHARS = [chr(i) for i in range(65, 65 + 26)] + [chr(i) for i in range(97, 97 + 26)] + [chr(i) for i in
+                                                                                                  range(48,
+                                                                                                        48 + 10)] + SYMBOLS + WHITESPACES
+
+
 class TokenType:
     NUM = 'NUM'
     ID = 'ID'
@@ -10,7 +14,7 @@ class TokenType:
     SYMBOL = 'SYMBOL'
     COMMENT = 'COMMENT'
     WHITESPACE = 'WHITESPACE'
-    EOF='EOF'
+    EOF = 'EOF'
 
     def __init__(self):
         pass
@@ -23,33 +27,33 @@ class Token:
 
 
 class Scanner:
-    def __init__(self,input_file_name,tokens_file_name,errors_file_name,symbol_table_file_name):
-        self.input_file_name=input_file_name
-        self.tokens_file_name=tokens_file_name
-        self.errors_file_name=errors_file_name
-        self.symbol_table_file_name=symbol_table_file_name
+    def __init__(self, input_file_name, tokens_file_name, errors_file_name, symbol_table_file_name):
+        self.input_file_name = input_file_name
+        self.tokens_file_name = tokens_file_name
+        self.errors_file_name = errors_file_name
+        self.symbol_table_file_name = symbol_table_file_name
         try:
-            self.text=open(self.input_file_name, 'r').read()
+            self.text = open(self.input_file_name, 'r').read()
         except:
             raise Exception(f'There is no file in this directory called {self.input_file_name}.')
         self.pointer = 0
         self.current_token_string = ''
         self.eof_pointer = len(self.text)
         self.line_number = 1
-        self.comment_start_line=None
+        self.comment_start_line = None
         self.scan_is_ended = False
         self.q_state = 0
 
-        self.tokens_table=''
-        self.errors_table=''
-        self.symbols_table='\n'.join([f'{i+1}.\t{KEYWORDS[i]}' for i in range(len(KEYWORDS))])+'\n'
-        self.symbols=KEYWORDS.copy()
-        self.symbols_table_counter=len(KEYWORDS)+2
+        self.tokens_table = ''
+        self.errors_table = ''
+        self.symbols_table = '\n'.join([f'{i + 1}.\t{KEYWORDS[i]}' for i in range(len(KEYWORDS))]) + '\n'
+        self.symbols = KEYWORDS.copy()
+        self.symbols_table_counter = len(KEYWORDS) + 2
 
-    def move_pointer_back(self,current_character):
-        self.pointer-=1
-        if current_character=='\n':
-            self.line_number-=1
+    def move_pointer_back(self, current_character):
+        self.pointer -= 1
+        if current_character == '\n':
+            self.line_number -= 1
 
     def switch_case_on_q0_state(self, current_character):
         token_found_bool = False
@@ -60,91 +64,64 @@ class Scanner:
         elif 65 <= ord(current_character) <= 65 + 25 or 97 <= ord(current_character) <= 97 + 25:
             self.q_state = 3
             self.current_token_string += current_character
-        elif current_character in [';', ':', ',', '[', ']', '(', ')', '{', '}']:
+        elif current_character in [';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '<']:
             self.current_token_string += current_character
             token_found_bool = True
             found_token = Token(TokenType.SYMBOL, self.current_token_string)
-        elif current_character=='+':
-            self.q_state=14
+        elif current_character == '*':
+            self.q_state = 16
             self.current_token_string += current_character
-        elif current_character=='-':
-            self.q_state=17
-            self.current_token_string += current_character
-        elif current_character=='*':
-            self.q_state=20
-            self.current_token_string += current_character
-        elif current_character=='<':
-            self.q_state=23
-            self.current_token_string += current_character
-        elif current_character=='>':
-            self.q_state=26
-            self.current_token_string += current_character
-        elif current_character=='=':
-            self.q_state=29
+        elif current_character == '=':
+            self.q_state = 18
             self.current_token_string += current_character
         elif current_character in [' ', '\n', '\r', '\t', '\v', '\f']:
             token_found_bool = True
-            # We create the token here, and set token_found_bool = True, but the method that actually called this should take care not to take this token seriously. :)
             self.current_token_string += current_character
             found_token = Token(TokenType.WHITESPACE, self.current_token_string)
         elif current_character == '/':
-            self.q_state = 32
+            self.q_state = 21
             self.current_token_string += current_character
 
-        return token_found_bool, found_token
-
-    def switch_case_on_half_symbol_states(self,
-                                          current_character):  # use this when currently q_state is in [14, 17, 20, 23, 26, 29]
-        if current_character == '=':
-            self.current_token_string += current_character
-            token_found_bool = True
-            found_token = Token(TokenType.SYMBOL, self.current_token_string)  # self.current_token_string = '+=' or '-=' or '*=' or '<=' or '>='
-        else:
-            token_found_bool = True
-            found_token = Token(TokenType.SYMBOL, self.current_token_string)  # self.current_token_string = '+' or '-' or '*' or '<' or '>' or '='
-            self.move_pointer_back(current_character)
         return token_found_bool, found_token
 
     def switch_case_on_comments(self,
                                 current_character):  # this part checks both for comments and misleading symbols, /=
         token_found_bool = False
         found_token = None
-        if self.q_state == 32:
+        if self.q_state == 21:
             if current_character == '/':
                 self.current_token_string += current_character
-                self.q_state = 37
+                self.q_state = 25
             elif current_character == '*':
                 self.current_token_string += current_character
-                self.q_state = 35
+                self.q_state = 23
                 self.comment_start_line = self.line_number  # save the line number of start of the comment
-            elif current_character == '=':
-                self.current_token_string += current_character
-                token_found_bool = True
-                found_token = Token(TokenType.SYMBOL, self.current_token_string)  # self.current_token_string = '/='
             else:
                 token_found_bool = True
                 found_token = Token(TokenType.SYMBOL, self.current_token_string)  # self.current_token_string = '/'
                 self.move_pointer_back(current_character)
-        elif self.q_state == 35:
+        elif self.q_state == 23:
             if current_character == '*':
-                self.q_state = 36
+                self.q_state = 24
                 self.current_token_string += current_character
             else:
                 self.current_token_string += current_character
-        elif self.q_state == 36:
+        elif self.q_state == 24:
             if current_character == '/':
                 self.current_token_string += current_character
                 token_found_bool = True
                 found_token = Token(TokenType.COMMENT,
                                     self.current_token_string)  # self.current_token_string = '/* some comments */'
-            else:
-                self.q_state = 35
+            elif current_character == '*':
                 self.current_token_string += current_character
-        elif self.q_state == 37:
+            else:
+                self.q_state = 23
+                self.current_token_string += current_character
+        elif self.q_state == 25:
             if current_character == '\n':  # or EOF
                 token_found_bool = True
                 found_token = Token(TokenType.COMMENT,
-                                    self.current_token_string)  # self.current_token_string = '/* some comments */'
+                                    self.current_token_string)  # self.current_token_string = '// some comments'
             else:
                 self.current_token_string += current_character
         return token_found_bool, found_token
@@ -173,6 +150,23 @@ class Scanner:
             self.move_pointer_back(current_character)
         return token_found_bool, found_token
 
+    def switch_case_on_q16_state(self, current_character):
+        token_found_bool = True
+        found_token = Token(TokenType.SYMBOL, self.current_token_string)
+        self.move_pointer_back(current_character)
+        return token_found_bool, found_token
+
+    def switch_case_on_q18_state(self, current_character):
+        if current_character == '=':
+            self.current_token_string += current_character
+            token_found_bool = True
+            found_token = Token(TokenType.SYMBOL, self.current_token_string)  # self.current_token_string = '=='
+        else:
+            token_found_bool = True
+            found_token = Token(TokenType.SYMBOL, self.current_token_string)  # self.current_token_string = '='
+            self.move_pointer_back(current_character)
+        return token_found_bool, found_token
+
     def consider_token_at_eof(self):
         # Here, we don't put token_found_bool = True. We assume the method that called this already knows we are at EOF, so token_found_bool = True
         # Here, we don't self.pointer -= 1. We assume the method that called this already knows self.pointer >= len(self.text) so it won't read next characters, there isn't any.
@@ -181,13 +175,13 @@ class Scanner:
         elif self.q_state == 3:
             found_token = Token(TokenType.KEYWORD if self.current_token_string in KEYWORDS else TokenType.ID,
                                 self.current_token_string)  # ID or KEYWORD
-        elif self.q_state in [14, 17, 20, 23, 26, 29, 32]:
-            found_token = Token(TokenType.SYMBOL, self.current_token_string)  # SYMBOL
-        elif self.q_state in [35, 36, 37]:
+        elif self.q_state == 21:
+            found_token = Token(TokenType.SYMBOL, self.current_token_string)  # SYMBOL: '/'
+        elif self.q_state in [23, 24, 25]:
             found_token = Token(TokenType.COMMENT,
                                 self.current_token_string)  # This means self.current_token_string = '/*', or '/**' or '//' we are assuming this is COMMENT
         else:
-            found_token=Token(TokenType.EOF,'$')
+            found_token = Token(TokenType.EOF, '$')
         return found_token
 
     def get_next_token(self) -> Token:
@@ -196,13 +190,12 @@ class Scanner:
         self.current_token_string = ''
         found_token = None
         while not token_found_bool:
-            # print(self.q_state)
             if self.pointer >= self.eof_pointer:
                 # Here, we should check that the last token be automatically taken as a terminating state.
                 # e.g. If " abc" is at the end of document, we get stuck in q_state = 3 and "abc" will not be saved as a token.
                 self.handle_error(None)
                 found_token = self.consider_token_at_eof()
-                if found_token.type==TokenType.EOF:
+                if found_token.type == TokenType.EOF:
                     self.save_tokens_table()
                     self.save_errors_table()
                     self.save_symbols_table()
@@ -210,12 +203,17 @@ class Scanner:
                 token_found_bool = True
             else:
                 current_character = self.text[self.pointer]
-                error_happened=self.handle_error(current_character)
+                error_happened = self.handle_error(current_character)
                 if error_happened:
                     continue
                 if current_character == '\n':
                     self.line_number += 1
-                if self.q_state == 1:
+                if self.q_state == 0:
+                    terminating_state, terminating_token = self.switch_case_on_q0_state(current_character)
+                    if terminating_state:
+                        token_found_bool = True
+                        found_token = terminating_token
+                elif self.q_state == 1:
                     terminating_state, terminating_token = self.switch_case_on_q1_state(current_character)
                     if terminating_state:
                         token_found_bool = True
@@ -225,18 +223,18 @@ class Scanner:
                     if terminating_state:
                         token_found_bool = True
                         found_token = terminating_token
-                elif self.q_state in [14, 17, 20, 23, 26, 29]:
-                    terminating_state, terminating_token = self.switch_case_on_half_symbol_states(current_character)
+                elif self.q_state == 16:
+                    terminating_state, terminating_token = self.switch_case_on_q16_state(current_character)
                     if terminating_state:
                         token_found_bool = True
                         found_token = terminating_token
-                elif self.q_state in [32, 35, 36, 37]:
+                elif self.q_state == 18:
+                    terminating_state, terminating_token = self.switch_case_on_q18_state(current_character)
+                    if terminating_state:
+                        token_found_bool = True
+                        found_token = terminating_token
+                elif self.q_state in [21, 23, 24, 25]:
                     terminating_state, terminating_token = self.switch_case_on_comments(current_character)
-                    if terminating_state:
-                        token_found_bool = True
-                        found_token = terminating_token
-                else:  # entering this case means we are in q_state = 0
-                    terminating_state, terminating_token = self.switch_case_on_q0_state(current_character)
                     if terminating_state:
                         token_found_bool = True
                         found_token = terminating_token
@@ -245,7 +243,7 @@ class Scanner:
             if found_token.type not in [TokenType.WHITESPACE, TokenType.COMMENT]:
                 # Because we want to update self.tokens_table, we should exclude COMMENT and WHITESPACE
                 self.update_tokens_table(found_token)
-                if found_token.type ==TokenType.ID and found_token.string not in self.symbols:
+                if found_token.type == TokenType.ID and found_token.string not in self.symbols:
                     self.update_symbols_table(found_token.string)
         return found_token
 
@@ -254,59 +252,44 @@ class Scanner:
             self.tokens_table += f' ({token.type}, {token.string})'
         else:
             if self.tokens_table:
-                self.tokens_table+='\n'
+                self.tokens_table += '\n'
             self.tokens_table += f'{self.line_number}.\t({token.type}, {token.string})'
 
-    def update_errors_table(self, string, error_message,line_number=None):
+    def update_errors_table(self, string, error_message, line_number=None):
         if not line_number:
-            line_number=self.line_number
+            line_number = self.line_number
         if len(string) > 7:
             string = string[:7] + '...'
         if self.errors_table.split('\n')[-1].startswith(str(line_number)):
             self.errors_table += f' ({string}, {error_message})'
         else:
             if self.errors_table:
-                self.errors_table+='\n'
+                self.errors_table += '\n'
             self.errors_table += f'{line_number}.\t({string}, {error_message})'
 
     def update_symbols_table(self, string):
         self.symbols_table += f'{self.symbols_table_counter}.\t{string}\n'
-        self.symbols_table_counter+=1
+        self.symbols_table_counter += 1
         self.symbols.append(string)
 
-    def handle_error(self,current_character):
+    def handle_error(self, current_character):
 
         # check for unclosed comment error
-        if current_character is None and self.q_state in [35,36]:
-            self.update_errors_table(self.current_token_string,'Unclosed comment',self.comment_start_line)
+        if current_character is None and self.q_state in [23, 24]:
+            self.update_errors_table(self.current_token_string, 'Unclosed comment', self.comment_start_line)
         elif current_character is None:
             return False
         # check for Invalid input
         # note that there could be invalid input inside of a comment! :)
-        if self.q_state not in [35,36,37] and current_character not in ACCEPTABLE_CHARS:
-            #todo: pooya check Test 11
-
-            # If the invalid input is after something but not a symbol like 'abc#' we should report 'abc#' as the thrown out part
-            # if self.q_state not in [14,17,20,23,26,29]:
-            #     self.update_errors_table(self.current_token_string + current_character, 'Invalid input')
-            #     self.pointer += 1
-            #     self.q_state = 0
-            #     self.current_token_string = ''
-            #     return True
-            # But if the invalid input is after a symbol like '/^' we should just report '^' as the invalid input and recognize the symbol token.
-            # so we don't raise the error and after a backtrack in the next iteration '^' is considered as an invalid input
-            # else:
-            #     return False
-
+        if self.q_state not in [23, 24, 25] and current_character not in ACCEPTABLE_CHARS:
             self.update_errors_table(self.current_token_string + current_character, 'Invalid input')
             self.pointer += 1
             self.q_state = 0
             self.current_token_string = ''
             return True
 
-
         # check for Invalid number (eg: 125d)
-        if self.q_state==1 and (65 <= ord(current_character) <= 65 + 25 or 97 <= ord(current_character) <= 97 + 25):
+        if self.q_state == 1 and (65 <= ord(current_character) <= 65 + 25 or 97 <= ord(current_character) <= 97 + 25):
             self.update_errors_table(self.current_token_string + current_character, 'Invalid number')
             self.pointer += 1
             self.q_state = 0
@@ -314,7 +297,7 @@ class Scanner:
             return True
 
         # check for unmatched comment
-        if self.q_state == 20 and current_character == '/':
+        if self.q_state == 16 and current_character == '/':
             self.update_errors_table(self.current_token_string + current_character, 'Unmatched comment')
             self.pointer += 1
             self.q_state = 0
--- compiler.py
+++ compiler.py
@@ -1,10 +1,9 @@
 # This is a c-minus compiler program written by
 # Parham Bateni     Student number: 99105294
 # Pooya EsmaeilAkhoondi       Student number: 99109303
-# Alireza Norouzi       Student number:
 # as the project of Compiler Design course held at Sharif University of Technology at Fall semester of 2022-2023
+# More details on the project can be found at https://github.com/My-Sharif-Projects/Compiler-Project-Fall-2022-2023
 from Scanner import Scanner
-from Scanner import Token
 from Scanner import TokenType
 
 INPUT_FILE_NAME = 'input.txt'
@@ -14,14 +13,14 @@ SYMBOL_TABLE_FILE_NAME = 'symbol_table.txt'
 
 if __name__ == '__main__':
     try:
-        scanner = Scanner(INPUT_FILE_NAME,TOKENS_FILE_NAME,ERRORS_FILE_NAME,SYMBOL_TABLE_FILE_NAME)
+        scanner = Scanner(INPUT_FILE_NAME, TOKENS_FILE_NAME, ERRORS_FILE_NAME, SYMBOL_TABLE_FILE_NAME)
     except Exception as e:
         print(e)
         print('Compiling failed!')
         exit()
     while True:
         token = scanner.get_next_token()
-        if token.type!=TokenType.EOF:
+        if token.type != TokenType.EOF:
             # parse the token to parser in the next phase
             pass
         else:
